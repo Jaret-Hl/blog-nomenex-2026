fe9872a4b15c160c2062d95e5eea2b5bbdb31ecc
Jaret <104955952+Jaret-Hl@users.noreply.github.com>
2026-01-15 11:03:07 -0600

    Remove console log for sanitized data in PostForm component

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -24,7 +24,7 @@ export default function PostForm() {
     };
     
     // Enviar datos sanitizados
-    console.log('Datos sanitizados:', sanitizedData);
+    
     // Aquí puedes hacer el POST a tu API
   };
 
